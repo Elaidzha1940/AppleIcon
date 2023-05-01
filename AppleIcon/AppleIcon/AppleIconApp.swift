11d09d1cca7297d8d6a1bce05c5ae7a30938f51d
Elaidzha1940 <eli080293@gmail.com>
2023-05-01 23:38:36 +0700

    AppleIcon

---
+++ AppleIcon/AppleIcon/AppleIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleIconApp.swift
+//  AppleIcon
+//
+//  Created by Elaidzha Shchukin on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppleIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
